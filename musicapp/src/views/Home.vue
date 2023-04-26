e36f08d03461c501006c4d73641927774914b3f8
yisongyang <yisongyang@nuzarsurf.com>
2023-04-26 17:04:37 +0800

    轮播图使用swiper组件

--- musicapp/src/views/Home.vue
+++ musicapp/src/views/Home.vue
@@ -1,19 +1,52 @@
 <template>
-  <top-nav></top-nav>
+  <div class="home">
+    <top-nav></top-nav>
+    <swiper class="swiper-container"
+        :pagination="{ clickable: true }"
+        :autoplay="{ autoplay: true }"
+        loop>
+        <swiper-slide><img src="@/assets/banner.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/banner.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/banner.png" alt=""></swiper-slide>
+        
+    </swiper>
+  </div> 
 </template>
 
 <script>
 // @ is an alias to /src
 import topNav from '@/components/topNav.vue'
 
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import "swiper/swiper-bundle.css";
+
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay])
+
 export default {
   name: 'Home',
   components: {
-    topNav
+    topNav,
+    Swiper,
+    SwiperSlide
   }
 }
 </script>
 
-<style scoped>
-
+<style>
+.swiper-container{
+  width: 7.1rem;
+  height: 3rem;
+  border-radius: 0.1rem;
+}
+.swiper-pagination-bullet-active{
+  background-color: red;
+}
 </style>
