504756a5209142447f3527e347fac8f55f634ea5
yisongyang <yisongyang@nuzarsurf.com>
2023-05-24 17:38:35 +0800

    P56-marquee标签实现跑马灯标题

--- musicapp/src/components/playMusic.vue
+++ musicapp/src/components/playMusic.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="playMusic">
         <div class="bg" :style="{backgroundImage:  `url(${playDetail.al.picUrl})`  }"></div>
-        <div class="playTop"></div>
+        <div class="playTop">
+            <div class="back" @click="$emit('back')">
+                <i class="iconfont icon-zuojiantou"></i>
+            </div> 
+            <div class="center">
+                <div class="title">
+                    <marquee behavior="scroll" direction="left"> {{ playDetail.al.name }}</marquee>
+                </div>
+            </div>
+            <div class="share">
+                <i class="iconfont icon-fenxiang"></i>
+            </div> 
+        </div>
         <div class="playContent"></div>
         <div class="playLyric"></div>
         <div class="progress"></div>
@@ -32,6 +44,25 @@ export default{
         background-size: auto 100%;
         background-position: center;
         filter: blur(60px);
+    }
+    .playTop{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 7.5rem;
+        height: 1.2rem;
+        color: #fff;
+        padding: 0 0.4rem;
+        .iconfont{
+            font-size: 32px;
+            color: #fff;
+        }
+        marquee{
+            width: 5rem;
+        }
     }
 }
 </style>
